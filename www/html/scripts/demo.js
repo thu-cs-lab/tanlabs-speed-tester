ec87c28968c337e3602930c23370a350fd1fb748
Rick Ho <laekov.h@gmail.com>
2020-11-04 16:50:11 +0800

    js support for demo steps and status display

--- www/html/scripts/demo.js
+++ www/html/scripts/demo.js
@@ -2,9 +2,10 @@ const DemoApp = {
 	data() {
 		return { 
 			show_ctrl: true,
-			report_title: 'Test pending',
+			report_title: 'Test pending, no result',
 			n_rip: '5, 50, 500, 5000, 50000',
 			n_packet: '0, 1, 2, 4, 16, 64, 128, 512, 1500',
+			current_status: '',
 			conn_cases: [
 				[3, 4, 1, 2],
 				[2, 3, 4, 1],
@@ -27,6 +28,9 @@ const DemoApp = {
 			} else {
 				this.conn_cases.push(this.conn_cases[this.conn_cases.length - 1]);
 			}
+		},
+		demoStep() {
+			this.current_status += 'Testing';
 		}
 	},
 	mounted() {
