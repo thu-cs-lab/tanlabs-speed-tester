791fd5ede468c32dda34aea4ea36f8dd2dcd72e0
Rick Ho <laekov.h@gmail.com>
2020-12-04 21:28:18 +0800

    web server provides static files

--- backend/src/main.cc
+++ backend/src/main.cc
@@ -1,2 +1,10 @@
+#include <tst/web_server.hh>
+#include <tst/test_functions.hh>
+
+
 int main() {
+	// tst_setup();
+
+	WebServer server;
+	server.listen("0.0.0.0", "8823");
 }
---
+++ backend/src/web_server.cc
@@ -0,0 +1,69 @@
+#include <vector>
+#include <string>
+#include <unistd.h>
+
+#include <tst/web_server.hh>
+#include <CivetServer.h>
+
+using std::string;
+
+#define TST_LOG(...) fprintf(stderr, __VA_ARGS__)
+
+class APIHandler: public CivetHandler {
+public:
+	bool handleGet(CivetServer *server, struct mg_connection *conn) {
+		const struct mg_request_info *req_info = mg_get_request_info(conn);
+
+		TST_LOG("GET request: %s\n", req_info->local_uri);
+
+		string uri(req_info->local_uri);
+		
+		if (uri == "/api/get_busy") {
+		} else if (uri == "/api/get_duration") {
+		} else if (uri == "/api/get_result") {
+		} else {
+			return false;
+		}
+
+		return true;
+	}
+
+	bool handlePost(CivetServer *server, struct mg_connection *conn) {
+		const struct mg_request_info *req_info = mg_get_request_info(conn);
+
+		TST_LOG("POST request: %s\n", req_info->local_uri);
+
+		string uri(req_info->local_uri);
+		
+		if (uri == "/api/test_rip") {
+		} else if (uri == "/api/test_connectivity") {
+		} else if (uri == "/api/test_bandwidth") {
+		} else {
+			return false;
+		}
+
+		return true;
+	}
+};
+
+
+void WebServer::listen(const char* addr, const char* port) {
+	mg_init_library(0);
+
+	const char *options[] = { 
+		"document_root", "../www/html", 
+		"listening_ports", port, 0};
+
+	std::vector<std::string> cpp_options;
+	for (int i=0; i<(sizeof(options)/sizeof(options[0])-1); i++) {
+		cpp_options.push_back(options[i]);
+	}
+	CivetServer server(cpp_options);
+
+	APIHandler h_api;
+	server.addHandler("/api*", h_api);
+	while (true) {
+		sleep(60);
+	}
+	mg_exit_library();
+}
