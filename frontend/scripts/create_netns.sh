788a4bda900d19a4e5403455280f3f3f7a6ef8c3
Rick Ho <laekov.h@gmail.com>
2020-12-14 15:15:16 +0800

    tc bw limit and view bird scripts

--- frontend/scripts/create_netns.sh
+++ frontend/scripts/create_netns.sh
@@ -7,4 +7,13 @@ do
 	ip netns exec vn$i ip link set eth$i up
 	ip netns exec vn$i ip addr add 10.0.$(expr $i - 1).2/24 dev eth$i 
 	ip netns exec vn$i ifconfig eth$i multicast
+	IFACE=eth$i
+	ip netns exec vn$i tc qdisc del dev ${IFACE} root
+	ip netns exec vn$i tc qdisc add dev ${IFACE} root handle 1:0 htb default 1
+	ip netns exec vn$i tc class add dev ${IFACE} parent 1:0 classid 1:1 htb rate 1.5Mbit ceil 1.5Mbit
+	ip netns exec vn$i tc filter add dev ${IFACE} parent 1:0 prio 1 protocol ip u32 match ip dport 520 0xffff flowid 1:1
+
+	ip netns exec vn$i tc -s qdisc ls dev ${IFACE}
+	ip netns exec vn$i tc -s class ls dev ${IFACE}
+	ip netns exec vn$i tc -s filter ls dev ${IFACE}
 done
---
+++ frontend/scripts/viewbirds.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in 1 2 3 4 
+do 
+	echo show route | birdcl -s /var/run/bird$i.ctl | wc -l
+done
